74e289ac45ff3df77a6e2d6027bf7c6cb37a0a15
Mario Codes Sánchez <m.sanchez@neusta.de>
2018-03-16 10:45:05 +0100

    Copied basic template

---
+++ src/main/java/es/msanchez/project/Starter.java
@@ -0,0 +1,12 @@
+package es.msanchez.project;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Starter {
+
+	public static void main(String[] args) {
+		log.info("Main started.");
+	}
+
+}
